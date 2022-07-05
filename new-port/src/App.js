9796b0c93300efd159261e95ecd75c41efb49800
Matthew Cross <matthewcross1017@gmail.com>
2022-07-05 16:50:33 -0400

    restart app and add tailwind

---
+++ new-port/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Project";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
+
+export default function App() {
+  return (
+    <main className="text-gray-900 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
+}
